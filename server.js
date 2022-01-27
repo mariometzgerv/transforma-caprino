dde71d0ef54eeb6214d9f5b2e4e7e211100a3dd6
Mario Metzger <mariometzgerv@gmail.com>
2022-01-27 11:33:19 -0300

    initial commit

--- server.js
+++ server.js
@@ -2,14 +2,9 @@ const express   = require('express');
 const path      = require('path');
 const app       = express();
 
-/*
 app.use(express.static(__dirname +'/dist/transforma-caprino'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +'/dist/transforma-caprino/index.html'));
 });
-*/
-app.use(express.static('./dist/transforma-caprino'));
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', { root: 'dist/transforma-caprino/' });
-});
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 5000);
