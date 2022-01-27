c7c809b7767a14bce4aba55b617c5d980114f976
Mario Metzger <mariometzgerv@gmail.com>
2022-01-27 11:55:20 -0300

    initial commit

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express   = require('express');
 const path      = require('path');
 const app       = express();
 
-app.use(express.static(__dirname +'/dist/transforma-caprino'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/transforma-caprino'));
+
+// Send all requests to index.html
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname +'/dist/transforma-caprino/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/transforma-caprino/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
